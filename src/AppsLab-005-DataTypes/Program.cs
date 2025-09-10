ee168fc4208a9cb17fae57fa73f39aa8c9ad376d
khanchenkod25 <khanchenkod25@U00T1-01>
2025-09-10 10:04:48 +0200

    Skusanie premennych

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 15;
+            Console.WriteLine(myAge);
+
+            float pi = 3.14f;
+            Console.WriteLine(pi);
+
+            string name = "David";
+            Console.WriteLine("David");
+
+            char gender = 'M';
+            Console.WriteLine('M');
+
+            bool isChild = true;
+            Console.WriteLine(true);
+        }
+    }
+}
