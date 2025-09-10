5b07987ae02c0a36ad83c68a992cd476065a65fd
khanchenkod25 <khanchenkod25@U00T1-01>
2025-09-10 13:09:22 +0200

    scitanie odcitanie

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Reflection.Metadata;
 
 namespace MyApp
 {
@@ -7,7 +8,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int myAge = 15;
+            /*int myAge = 15;
             Console.WriteLine(myAge);
 
             float pi = 3.14f;
@@ -20,7 +21,51 @@ namespace MyApp
             Console.WriteLine('M');
 
             bool isChild = true;
-            Console.WriteLine(true);
+            Console.WriteLine(true);*/
+
+            /*int a = 10;
+            int b = 5;
+
+            int sum = a + b;
+            Console.WriteLine(sum);
+            //scitavanie a b
+
+            Console.WriteLine(sum);
+            Console.WriteLine(a - b);
+            Console.WriteLine(10 - 5);
+            //odcitavanie a b
+
+            Console.WriteLine(a*b); //nasobenie 
+
+            Console.WriteLine(a/b); //delenie
+
+            Console.WriteLine("52");*/
+
+            /*int birthday = 20;
+            int birthmoth = 10;
+            int birthyear = 2009;
+
+            /*int birthsum = birthday + birthmoth + birthyear;
+            Console.WriteLine(birthsum);
+            Console.WriteLine(birthsum *10);
+
+            Console.WriteLine((birthday + birthmoth) * birthyear);
+
+            Console.WriteLine(5/3f);
+
+            string name = "David";
+            Console.WriteLine(name + birthyear);
+            Console.WriteLine(name + birthday + birthmoth);*/
+
+            bool result = 6 < 3;
+            Console.WriteLine(result);
+
+            bool resultt = 6 > 3;
+            Console.WriteLine(resultt);
+
+
+
+
         }
     }
 }
