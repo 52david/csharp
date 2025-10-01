5172c5e47689cfd5054a5087d7ec00e61fc95792
khanchenkod25 <khanchenkod25@U00T1-01>
2025-10-01 10:44:41 +0200

    Cykly

---
+++ Cvicenie_Cykly/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace Cvicenie_Cykly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Hello, World!");
+            Console.WriteLine(1);
+            Console.WriteLine(2);
+            Console.WriteLine(3);
+            Console.WriteLine(4);
+            Console.WriteLine(5);
+            Console.WriteLine(6);
+            Console.WriteLine(7);
+            Console.WriteLine(8);
+            Console.WriteLine(9);
+            Console.WriteLine(10);*/
+
+            /*for (int i = 0; i <= 50; i++)
+            {
+                Console.WriteLine(i+50); //vypisuje cisla od 50 do 100
+
+            }*/
+
+            /*for (int i = 100; i >=0; i--)
+            {
+                Console.WriteLine(i);   //vypisuje cisla od 100 do 0
+            }*/
+
+            /*while (true) {
+                Console.WriteLine("david"); //nekonecno vzpisuje
+
+           }*/
+
+            /* int i = 0;
+             while (i <= 100) { Console.WriteLine(); i++; } */
+
+            /*while (true) 
+              {
+                  string input = Console.ReadLine();
+
+                  if (input == "pozdrav")
+                  {
+                      Console.WriteLine("Ahoj");
+                  }
+                  else if (input == "exit")
+                  {
+                      break;
+                  }*/
+
+            /*while (true)
+            {
+                while (true )
+                {
+                    string input = Console.ReadLine(); 
+
+                    if ( input == "exit")
+                    {
+                        break;
+                    }
+                    Console.WriteLine("michal");
+
+                    string input2 = Console.ReadLine();
+                    if ( input2 == "koniec")
+                    {  
+                        break;
+                    }
+                    Console.WriteLine("Igor");
+                        
+                }
+            }*/
+
+            /*for (int i = 0; i <= 7; i++) //urobit trojuholnik z hviezdicek
+            {
+                string row = "";
+                for (int j = 0; j <= i; j++)
+                {
+                    row += "*";
+                    //row = row + "*"
+                }
+                Console.WriteLine(row);
+            }*/
+
+            /*Console.WriteLine("kolko riadkov");
+            int countofRows = int.Parse(Console.ReadLine());
+            Console.WriteLine("Aky znak");
+            string design = Console.ReadLine();
+            Console.WriteLine();
+            for (int i = 0; i < countofRows; i++)
+            { 
+                string row = "";
+                for (int j = 0; j <= i; j++)
+                {
+                    row += design;
+                }
+                Console.WriteLine(row);
+            }*/
+
+        }
+
+    }
+}
+
+   
