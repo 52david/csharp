1f44a194cd6f6b87822125dd59b6341a477ed657
khanchenkod25 <khanchenkod25@U00T1-01>
2025-10-01 12:53:45 +0200

    Polia

--- Cvicenie_Polia/Program.cs
+++ Cvicenie_Polia/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Cvicenie_Polia
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cvicenie_Polia
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*int[] numbers = new int[7];
+            numbers[0] = 10; 
+            numbers[1] = 15;
+            numbers[2] = 35;
+            numbers[3] = 40;
+            numbers[4] = 2;
+            numbers[5] = 1;
+            numbers[6] = 19;
+
+            /*int sum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+                //sum + sum = numbers[i]
+                Console.WriteLine(numbers);
+            }
+            Console.WriteLine(sum);*/
+
+            /*int sum = 0;
+            foreach (var item in numbers)
+            {
+                sum += item;
+            }
+            Console.WriteLine(sum);*/
+
+            /*int[] numbers = new int[5];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("zadaj cislo");
+                string number= Console.ReadLine();
+                int namber = int.Parse(number);
+            }
+            int sum = 0;
+            int namber = Console.ReadLine();
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(sum + namber);        
+            }
+            Console.WriteLine(sum);*/
+
         }
     }
 }
