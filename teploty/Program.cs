d3e67e705d1972160c5964495094ca3d7bf99de1
khanchenkod25 <khanchenkod25@U00T1-01>
2025-10-22 12:58:00 +0200

    konzol a cvicenie teploty

---
+++ teploty/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace teploty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*List<int> teploty = new List<int> { 3, -1, 12, 7, -3, 0, 19, 14, 2, 5, -5, 8 };
+            string riadok = "";
+            foreach (var teplota in teploty)
+            {
+                riadok += teplota + ", ";
+            }
+            Console.WriteLine(riadok);
+
+            int max = int.MinValue;
+            foreach (int teplota in teploty)
+            {
+                if (teplota > max)
+                {
+                    max = teplota;
+                }
+            }
+            Console.WriteLine("Maximalna teplota: "+max);
+
+            int min = int.MaxValue;
+            foreach (int teplota in teploty)
+            {
+                if (teplota < min) 
+                {
+                    min = teplota;
+                }
+            }
+            Console.WriteLine("Minimalna teplota: " +min);
+
+            int sum = 0;
+            foreach (int teplota in teploty)
+            {
+                sum += teplota;
+            }
+            int pocet = teploty.Count;
+            double avg = (double) sum/pocet;
+            Console.WriteLine("Priemer: "+avg);
+
+            int pocetnad = 0;
+            foreach (int teplota in teploty)
+            {
+                if (teplota > avg)
+                {
+                    pocetnad++;
+                }
+            }
+            Console.WriteLine("Nad priemerom je " + pocetnad + " cisel");
+
+            List<int> kladneTeploty = new List<int>();
+            foreach (int teplota in teploty)
+            {
+                if (teplota >= 0)
+                {
+                    kladneTeploty.Add(teplota);
+                }
+            }
+            Console.WriteLine("Kladne: ");
+            foreach (var i in kladneTeploty)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine();*/
+        }
+    }
+}
