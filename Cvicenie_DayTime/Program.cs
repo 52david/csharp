2b7e4093645755031abd4123f345164b82f050f0
khanchenkod25 <khanchenkod25@U00T1-01>
2026-02-11 13:13:09 +0100

    time

---
+++ Cvicenie_DayTime/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Cvicenie_DayTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            foreach (var s in args)
+            {
+                //
+                //
+                //
+            }
+            DateTime dt2 = DateTime.Now;
+            TimeSpan ts = dt2 - dt;
+            Console.WriteLine(ts.TotalMilliseconds);
+            DateTime birthday = new DateTime(year: 2009, month: 10, day: 20,hour: 8, minute: 29, second:26);
+            string datum = "20.10.2009";
+            DateTime dat = DateTime.Now;
+            Console.WriteLine(dat.ToString("dd.MM.yyyy"));
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DateTime startTime;
+        private DateTime stopTime;
+        private TimeSpan elapsed;
+        private bool runnig;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            startTime = DateTime.Now;
+
+            Button_Click.IsEnabled = false;
+            Button_Click_1.IsEnabled = true;
+
+            Label.Content = "";
+            TextBox.IsEnabled = false;
+            Button_Click_2.IsEnabled = false;
+            TextBox.Text = ""; 
+
+
+            
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            stopTime = DateTime.Now;
+
+            elapsed = stopTime - startTime;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
